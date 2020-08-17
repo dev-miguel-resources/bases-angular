5865f40a608c4ece1e3628bb4e0dd8253ca62716
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-08-17 13:06:15 -0400

    bases angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// de padre a hijo: puedo pasar texto plano, variables, objetos, arreglos
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bases-angular';
+
+  x = 'freezer!';
+  rpta: number = 0;
+
+  recibirResultado(e: any) {
+    this.rpta = e;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CompcComponent } from './compc/compc.component';
+import { GohanComponent } from './gohan/gohan.component';
+import { GokuComponent } from './goku/goku.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent,
+    CompaComponent,
+    CompbComponent,
+    CompcComponent,
+    GohanComponent,
+    GokuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
